ee8829f660e17abc9778e6211dbae2f41ed6f232
steffjenl <stephan@monkeysoft.nl>
2025-09-01 22:54:12 +0200

    Removing fetch and replace with http lib

--- drivers/tvoverlay/driver.js
+++ drivers/tvoverlay/driver.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Homey = require('homey');
-
 module.exports = class TvOverlayDriver extends Homey.Driver {
 
   /**
@@ -17,38 +16,54 @@ module.exports = class TvOverlayDriver extends Homey.Driver {
     session.setHandler("validateLoginData", async function (data) {
       that.homey.app.api.setSettings(data.ipAddress, data.port);
 
-      const res = await fetch(`http://${data.ipAddress}:${data.port}/get`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-
-      const resJson = await res.json();
-      if (resJson) {
+      return that.homey.app.api.getInfo().then(info => {
         devices = [
           {
-            name: resJson.result.settings.deviceName,
-            data: { id: resJson.result.status.id },
+            name: info.settings.deviceName,
+            data: { id: info.status.id },
             settings: {
               ip: data.ipAddress,
               port: data.port,
             }
           },
         ];
-        return resJson.result.settings.deviceName;
-      }
-      devices = [];
-      return false;
 
+        return info.settings.deviceName;
+      }).catch(err => {
+        that.log('Error getting device info:', err);
+        devices = [];
+        return false;
+      });
     });
 
     session.setHandler("list_devices", async function () {
       return devices;
+    });
+  }
+
+  async onRepair(session, device) {
+    let that = this;
+    let devices = [];
+    session.setHandler("validateLoginData", async function (data) {
+      that.homey.app.api.setSettings(data.ipAddress, data.port);
+
+      return that.homey.app.api.getInfo().then(info => {
+
+        const settings =  {
+              ip: data.ipAddress,
+              port: data.port,
+            };
+
+        device.setSettings(settings).catch(err => {
+          that.log('Error setting device settings:', err);
+        });
+
+        return info.settings.deviceName;
+      }).catch(err => {
+        that.log('Error getting device info:', err);
+        devices = [];
+        return false;
+      });
     });
   }
 
